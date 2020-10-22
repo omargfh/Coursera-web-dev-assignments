02e767ae7ebc53d259afe9e2e935718e2db6c3f7
unknown <18043@stemegypt.edu.eg>
2020-10-22 03:04:36 +0200

    CSA

--- CSA/js/script.js
+++ CSA/js/script.js
@@ -11,11 +11,24 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 	var searchBar = document.getElementById("search-bar");
 	var searchBarText = document.getElementById("search-bar-text");
+	var field = document.getElementById("search-query");
+
 
 	document.getElementById("search-bar-text").addEventListener("click", function() {
 		searchBarText.classList.add("hidden");
 		searchBar.classList.add("animate-search-bar");
+		field.classList.remove("hidden");
+		searchBar.classList.remove("deanimate-search-bar");
+		field.focus();
 	});
+
+
+	field.onblur = function() {
+		field.classList.add("hidden");
+		searchBar.classList.add("deanimate-search-bar");
+		searchBarText.classList.remove("hidden");
+		searchBar.classList.remove("animate-search-bar");
+	}
 
 	// document.querySelector("body").addEventListener("click", function() {
 	// 	searchBarText.classList.remove("hidden");
