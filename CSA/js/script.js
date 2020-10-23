5ed34ed093084fb6200db32f22ba5bbcdff6a73b
unknown <18043@stemegypt.edu.eg>
2020-10-24 01:43:04 +0200

    CSA update

--- CSA/js/script.js
+++ CSA/js/script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 	document.getElementById("nav-button").addEventListener("click", function() {
 	var element = document.getElementById("header-background");
 	var elementOther = document.getElementById("toggle-nav");
+	var content = document.getElementById("collapsable-nav");
 	element.classList.toggle("hidden");
 	elementOther.classList.toggle("hidden");
 	elementOther.classList.toggle("header");
@@ -10,8 +11,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
 	});
 
 	var searchBar = document.getElementById("search-bar");
+	var searchBarMobile = document.getElementById("search-bar-mobile");
 	var searchBarText = document.getElementById("search-bar-text");
+	var searchBarTextMobile = document.getElementById("search-bar-text-mobile");
 	var field = document.getElementById("search-query");
+	var fieldMobile = document.getElementById("search-query-mobile");
 
 
 	document.getElementById("search-bar-text").addEventListener("click", function() {
@@ -29,6 +33,32 @@ document.addEventListener("DOMContentLoaded", function (event) {
 		searchBarText.classList.remove("hidden");
 		searchBar.classList.remove("animate-search-bar");
 	}
+
+	document.getElementById("search-bar-text-mobile").addEventListener("click", function() {
+		searchBarTextMobile.classList.add("hidden");
+		fieldMobile.classList.remove("hidden");
+		fieldMobile.focus();
+	});
+
+
+	fieldMobile.onblur = function() {
+		fieldMobile.classList.add("hidden");
+		searchBarTextMobile.classList.remove("hidden");
+	}
+
+	var ig = document.getElementById("e-f");
+	var snapchat = document.getElementById("e-s");
+	var ig_logo = document.getElementById("logo-ig");
+	var snap_logo = document.getElementById("logo-snap");
+
+	document.getElementById("click-instagram").addEventListener("click", function() {
+		ig.classList.toggle("story-animate")
+		ig_logo.classList.toggle("hidden")
+	});
+	document.getElementById("click-snapchat").addEventListener("click", function() {
+		snapchat.classList.toggle("story-animate")
+		snap_logo.classList.toggle("hidden")
+	});
 
 	// document.querySelector("body").addEventListener("click", function() {
 	// 	searchBarText.classList.remove("hidden");
